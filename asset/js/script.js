66beb15fd96f31a5dbf4d3d2a9279532b1c9f160
Unirions <64539568+Corentin-catteau@users.noreply.github.com>
2025-11-15 18:35:52 +0100

    grid + keyBindings_Player2

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,17 +1,50 @@
-const canvas = document.getElementById('canvas_dessin');
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('canvas_dessin');
+    DrawGrid(canvas);
+});
 
-canvas.width = window.innerWidth * 0.9;
-canvas.height = window.innerHeight * 0.8;
-
-var keyBindings = {
-    up : "ArrowUp",
-    down : "ArrowDown",
-    left : "ArrowLeft",
-    right : "ArrowRight",
+var keyBindings_PLayer1 = {
+    up : "z",
+    down : "w",
+    left : "d",
+    right : "a",
     jump : "Space",
 }
 
+var keyBindings_Player2 = {
+    up : "m",
+    down : "k",
+    left : "o",
+    right : ";",
+    jump : "Space",
+}
 
+function DrawGrid(canvas){
+    const ctx = canvas.getContext('2d');
+
+    const pointSize = 10
+    const width = canvas.width;
+    const height = canvas.height;
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1
+
+    //Pour les lignes verticales
+    for(let x = 0; x <= width; x += pointSize){
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+        ctx.stroke();
+    }
+
+    //Pour les lignes horizontales
+    for(let y = 0; y <= height; y += pointSize){
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.stroke();
+    }
+}
 
 function ouvrirPlus(id) {
     document.getElementById(id).style.display = "flex";
